57a8651d855f49a5b92ab6f793a207be0d08a68a
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-12-10 21:30:11 -0800

    Add ilios eslint rules and ember recommendations

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,26 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended'
+  ],
   env: {
     browser: true
   },
   rules: {
+    "comma-dangle": [2, "only-multiline"],
+    "radix": [2, "always"],
+    /* Stylistic Issues http://eslint.org/docs/rules/#stylistic-issues */
+    indent: [2, 2], /* two-space indentation */
+    semi: 2, /* require semi-colons */
+    camelcase: 2, /* require camelCase variables */
+    'no-shadow': [2, {
+      builtinGlobals: true,
+    }],
+    'ember/order-in-components': 2,
+    'ember/order-in-controllers': 2,
+    'ember/order-in-models': 2,
+    'ember/order-in-routes': 2,
   }
 };
